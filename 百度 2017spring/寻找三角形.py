23ed9cd094748fc5db85e947ba92d23fe5e58c10
xybramble <15528053316@163.com>
2020-03-14 20:14:31 +0800

    Update 寻找三角形.py

--- 百度 2017spring/寻找三角形.py
+++ 百度 2017spring/寻找三角形.py
@@ -8,3 +8,16 @@ import math
 n = int(input())
 c_cords = []
 for i in range(n):
+  temp = input().split()
+  c_cords.append([temp[0]])
+  c_cords[i].extend([int(temp[j] for j in range(1, 4)])
+def get_distance(p1, p2):
+  return math.sqrt((p1[1]-p2[1])**2 + (p1[2]-p2[2])**2) + p1[3]-p2[3])**2)
+def get_area(p1, p2, p3):
+  a = get_distance(p1, p2)
+  b = get_distance(p2, p3)
+  c = get_distance(p1, p3)
+  p = (a+b+c)/2
+  t = p*(p-a)*(p-b)*(p-c)
+  if t>0:
+                     
