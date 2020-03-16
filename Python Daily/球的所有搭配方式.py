2492ab3626790a6ca4025274b1604604ab8d0e27
xybramble <15528053316@163.com>
2020-03-16 23:39:15 +0800

    Update 球的所有搭配方式.py

--- Python Daily/球的所有搭配方式.py
+++ Python Daily/球的所有搭配方式.py
@@ -1,3 +1,11 @@
 '''
 红球3个，黄球3个，绿球6个，把所有球放进一个盒子里面，从其中抽取8个球，求球的所有搭配方式
 '''
+for red in range(4):
+  for yellow in range(4):
+    for green in range(7):
+      if red + yellow + green == 8:
+        print("red = {},".format(red), end = "")
+        print("yellow = {},".format(yellow), end = "")
+        print("green = {},".format(green), end = "")
+        print()
