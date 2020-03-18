1be73fc42cdba9f95d70433c908bc23ce9a3b32f
xybramble <15528053316@163.com>
2020-03-18 11:42:57 +0800

    Update and rename python数据类型的内置函数(str,list,dict,set).py) to python数据类型的内置函数(str,list,dict,set).py

---
+++ Python Daily/python数据类型的内置函数(str,list,dict,set).py
@@ -0,0 +1,19 @@
+# python数据类型：str（字符串）, list（列表）, dict（字典）, set（集合）
+
+# 1.字符串连接操作
+
+s1 = "i love"
+s2 = "python"
+s3 = s1 + s2
+print(s3)
+print(s1 + "" + s2)
+'''
+运行结果：
+i lovepython
+i lovepython
+'''
+
+# 2.字符串的乘法
+
+s = "i love python"
+s2 = s * 10
