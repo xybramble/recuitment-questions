0c1f705dcd4d2f1580384cafde0991a113356081
xybramble <15528053316@163.com>
2020-03-22 00:05:14 +0800

    Create 76最小覆盖子串.py

---
+++ Microsoft暑期实习20spr/76最小覆盖子串.py
@@ -0,0 +1,6 @@
+'''
+https://www.nowcoder.com/discuss/379046?type=post&order=time&pos=&page=1
+寻找字符串包含自身所有元素的最小字串， 如aaabbbcc只包含a,b,c 三种，所以返回 字串 abbbc
+和leetcode第76题类似，但是要简单一些。
+面试的时候要想到滑动窗口双指针的思路，然后一步一步写出来run。
+'''
