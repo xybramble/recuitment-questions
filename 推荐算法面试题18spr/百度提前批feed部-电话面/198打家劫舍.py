4d392872c3434cc6329181b2b9433203c4c5ae6f
xybramble <15528053316@163.com>
2020-03-27 23:56:48 +0800

    Update 198打家劫舍.py

--- 推荐算法面试题18spr/百度提前批feed部-电话面/198打家劫舍.py
+++ 推荐算法面试题18spr/百度提前批feed部-电话面/198打家劫舍.py
@@ -1,1 +1,15 @@
 https://leetcode-cn.com/problems/house-robber
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        length = len(nums)
+        if length == 1:
+            return nums[0]
+        else:
+            prev = nums[0]
+            cur = max(prev, nums[1])
+            for i in range(2, length):
+                cur, prev = max(prev + nums[i], cur), cur
+            return cur
